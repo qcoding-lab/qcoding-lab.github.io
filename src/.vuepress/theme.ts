3e8a02f89e2640e7dd2a1ec310e0b9928e768342
qcoding-lab <qqzhwang@gmail.com>
2025-03-23 11:37:01 +0100

    change logo

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -11,7 +11,7 @@ export default hopeTheme({
     url: "https://mister-hope.com",
   },
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
+  logo: "/assets/images/logo1.png",
 
   repo: "vuepress-theme-hope/vuepress-theme-hope",
 
